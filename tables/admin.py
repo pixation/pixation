df563291c999da483b869bfa64c948e44e266d0a
Anand Singh Kunwar <anandkunwar95@gmail.com>
2016-11-10 04:04:00 +0530

    Added media table model

--- tables/admin.py
+++ tables/admin.py
@@ -1,7 +1,8 @@
 from tables.developer import Developer
 from tables.api_management import APIManagement
-
+from tables.media import Media
 from django.contrib import admin
 
 admin.site.register(Developer)
 admin.site.register(APIManagement)
+admin.site.register(Media)
