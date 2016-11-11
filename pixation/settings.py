d10ecdf09070948acfe2209ce3745b83534fc3b2
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-11-12 01:11:44 +0530

    Updated Media Model

--- pixation/settings.py
+++ pixation/settings.py
@@ -139,3 +139,6 @@ AUTHENTICATION_BACKENDS = (
 )
 
 SITE_ID = 1
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = os.path.join(BASE_DIR,'..','/media/')
--- tables/media.py
+++ tables/media.py
@@ -1,11 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
+
+def create_unique_filename(instance, filename):
+    extension = filename.split(".")[-1]
+    instance.old_filename = filename
+    instance.filename = "{}.{}".format(uuid.uuid4(),extension)
+    return instance.filename
 
 class Media(models.Model):
-    server = models.CharField(max_length=64)
-    path = models.CharField(max_length=64)
+    display_name = models.CharField(max_length=64)
     filename = models.CharField(max_length=64)
+    old_filename = models.CharField(max_length=64)
     owner = models.ForeignKey(User)
     width = models.IntegerField()
     height = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to=create_unique_filename,width_field=width, height_field=height)
