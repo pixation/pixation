1d787f063847ff30abd0c3eeb4873fda4ab6f1a0
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-11-11 23:44:47 +0530

    Changed route of accounts

--- pixation/urls.py
+++ pixation/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/',  admin.site.urls),
-    url(r'^accounts/', include('allauth.urls')),
+    url(r'', include('allauth.urls')),
 ]
