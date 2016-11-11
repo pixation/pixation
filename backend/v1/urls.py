41ef90cf990521193ccc0366ad12305fe9052002
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-11-12 04:36:03 +0530

    imposed user authentication over images

--- backend/v1/urls.py
+++ backend/v1/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from backend.v1.viewsets.media_viewsets import MediaUploadViewSet
+from backend.v1.viewsets.media_viewsets import MediaViewSet
 
 router = routers.DefaultRouter()
-router.register('images/upload', MediaUploadViewSet)
+router.register('images', MediaViewSet)
 urlpatterns = [
     url(r'^v1/', include(router.urls)),
 ]
-print(router.urls)
