93ec572afacdef2a301c47803460b2c6e1aa8892
Anand Singh Kunwar <anandkunwar95@gmail.com>
2016-11-15 02:04:03 +0530

    Added image manipulation frontend api

--- frontend/static/app/js/services.js
+++ frontend/static/app/js/services.js
@@ -90,9 +90,20 @@ angular.module('pixation.services', [])
     }])
     .factory('image', ["$location", "$http", "$log", "$q", function ($location, $http, $log, $q) {
         return {
-            imageAPI: function (data) {
+            imageAPIResize: function (data) {
                 var deferred = $q.defer();
-                var urlToUse = baseUrl + '/api/v1/getUserDashboardImages';
+                var urlToUse = baseUrl + '/api/v1/resize';
+                console.log(data);
+                $http.get(urlToUse, data).success(function (data) {
+                    deferred.resolve(data);
+                }).error(function (data) {
+                    deferred.reject();
+                });
+                return deferred.promise;
+            },
+            imageAPIRemoval: function (data) {
+                var deferred = $q.defer();
+                var urlToUse = baseUrl + '/api/v1/remove';
                 console.log(data);
                 $http.get(urlToUse, data).success(function (data) {
                     deferred.resolve(data);
