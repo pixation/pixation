3292ac411aeaac814203dc78e8e0410b18f44665
Anand Singh Kunwar <anandkunwar95@gmail.com>
2016-11-14 19:44:45 +0530

    Added image page complete

--- frontend/views.py
+++ frontend/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, redirect
+from tables.media import Media
+from django.contrib.auth.models import User
+from django.conf import settings
+from django.http import Http404
 
 # Create your views here.
 def dashboard(request):
@@ -15,7 +19,11 @@ def upload(request):
 
 def image(request, username, img):
     if request.user.is_authenticated():
-        # TODO Get Data
-        return render(request, 'image.html', context={'a':123})
+        query = Media.objects.filter(owner__username=username).filter(image=img).first()
+        if query is not None:
+            if (request.user == query.owner) or (query.public == True):
+                return render(request, 'image.html', context={'image': query.image.url, 'public': query.public, 'owner': query.owner.username})
+        # Else All
+        raise Http404
     else:
         return redirect('/login?next=/upload')
