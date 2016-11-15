dc4068e848fc791980ca353356d9a4cc09e2c436
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-11-15 09:27:29 +0530

    Added cron job

--- cronjobs/cronjobs.py
+++ cronjobs/cronjobs.py
@@ -1,5 +1,8 @@
 from django_cron import CronJobBase, Schedule
-
+from tables.cached_media import *
+from datetime import datetime, timedelta
+import os
+from django.conf import settings
 class MyCronJob(CronJobBase):
     RUN_EVERY_MINS = 0.1 # every 2 hours
 
@@ -7,5 +10,9 @@ class MyCronJob(CronJobBase):
     code = 'cronjobs.cronjobs.my_cron_job'    # a unique code
 
     def do(self):
-        print("HULA")
-        pass    # do your thing here
+        d = datetime.today() - timedelta(days=2)
+        cached_media = CachedMedia.objects.filter(last_hit_at__lte = d)
+        for query in cached_media:
+            name = str(query.image)
+            os.remove(os.path.join(settings.MEDIA_ROOT, name))
+            query.delete()
